ed645c1e25322126253a150c9c67137026ed3fd5
Максим Качмазов <maxim.kachmazov@bnovo.ru>
2026-05-01 19:25:33 +0300

    load shape refactored

--- scenarios/baseline_stepload/baseline_stepload_scenario.py
+++ scenarios/baseline_stepload/baseline_stepload_scenario.py
@@ -5,7 +5,12 @@ from core.load_shapes import StepLoadShape
 
 
 class BaselineStepLoadShape(StepLoadShape):
-    """Форма ступенчатой нагрузки для этого сценария (наследует StepLoadShape)."""
+    """Ступенчатая нагрузка для базового сценария."""
+
+    step_time = 180        # секунд на один шаг
+    users_per_step = 25    # пользователей добавляется на каждом шаге
+    spawn_rate = 5         # пользователей в секунду при спавне
+    max_steps = 5          # количество шагов до остановки теста
 
 
 class BaselineScenarioUser(BaseUser):
